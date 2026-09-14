cd51a310e42d7a679a409f9c386f289bb1f16fc3
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-14 16:04:00 +0900

    Refactor message condition with ternary operator

--- day-10/javascript-function.js
+++ day-10/javascript-function.js
@@ -7,12 +7,9 @@ const favorite = prompt("好きな動物は？");
 if (favorite) {
   const answer = confirm(`本当に${favorite}で良いですか？`);
 
-  if (answer) {
-    const message = favoriteAnimal(favorite);
-    console.log(message);
-  } else {
-    console.log("キャンセルされました");
-  }
+  const message = answer ? favoriteAnimal(favorite) : "キャンセルされました";
+
+  console.log(message);
 } else {
   console.log("好きな動物が入力されていません");
 }
