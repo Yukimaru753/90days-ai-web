02ac001fb29798ae2dfee91d6eb2e937a21a46e2
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-15 14:49:59 +0900

    Add sum problem solving practice

--- day-11/problem-solving.js
+++ day-11/problem-solving.js
@@ -3,11 +3,16 @@
 
 // Input:
 // 数字 n
+let n = 5;
 
 // Output:
 // 合計値
+let result;
 
 // Pseudocode:
 // resultを0にする
+result = 0;
 // resultにn(n+1)/2を代入
+result = n * (n + 1) / 2;
 // resultを返す
+console.log(result);
