1b661777d460bcb8e92aa59070bdda3d7536a08e
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-17 13:48:43 +0900

    Add .map(function)

--- day-13/javascript.js
+++ day-13/javascript.js
@@ -4,3 +4,10 @@ for (const cat of cats) {
     console.log(cat);
 }
 
+function toUpper(string) {
+  return string.toUpperCase();
+}
+
+const upperCats = cats.map(toUpper); //.map(関数)「配列catsの全要素を変換して、新しい配列upperCatsを作る」
+
+console.log(upperCats);
