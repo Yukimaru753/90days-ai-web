2c91b58bae1a235b4d2f89f321fa5d6e66f39a76
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-17 14:23:37 +0900

    Add .filter(function)

--- day-13/javascript.js
+++ day-13/javascript.js
@@ -4,6 +4,7 @@ for (const cat of cats) {
     console.log(cat);
 }
 
+//書き方１
 function toUpper(string) {
   return string.toUpperCase();
 }
@@ -11,3 +12,24 @@ function toUpper(string) {
 const upperCats = cats.map(toUpper); //.map(関数)「配列catsの全要素を変換して、新しい配列upperCatsを作る」
 
 console.log(upperCats);
+
+//書き方２
+const upperCats2 = cats.map((cat) => cat.toUpperCase()); //cats.map()に引数としてarrow functionを渡している
+
+console.log(upperCats2);
+
+//書き方１
+function lCat(cat) {
+  return cat.startsWith("L");
+}
+
+const filtered = cats.filter(lCat); //lCatがtrueだったら残す、falseだったら残さない
+
+console.log(filtered);
+
+//書き方２
+
+const filtered2 = cats.filter((cat) => cat.startsWith("L")); //returnは省略できる
+
+console.log(filtered2);
+
