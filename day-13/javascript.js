21f812cf466579f8604fed6efe871d4c28adf762
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-17 20:08:37 +0900

    Add array methods

--- day-13/javascript.js
+++ day-13/javascript.js
@@ -12,6 +12,7 @@ for (let i = 0; i < cats.length; i++) {
 
 console.log(cats.length);
 
+//map = 全員を変身させる
 //書き方１
 function toUpper(string) {
   return string.toUpperCase();
@@ -26,6 +27,7 @@ const upperCats2 = cats.map((cat) => cat.toUpperCase()); //cats.map()に引数�
 
 console.log(upperCats2);
 
+// filter = 条件で選抜する
 //書き方１
 function lCat(cat) {
   return cat.startsWith("L");
@@ -126,7 +128,6 @@ for (let i = 2; i <= 10; i++) {
 // 文字列の最後のカンマを消去
 // 文字列を表示
 
-
 let n = 10; //任意の数字
 let primeMessage = `n = ${n} `; //出力する文字列
 
@@ -135,7 +136,8 @@ for (let i = 2; i <= n; i++) {
   let isPrime = true; // 素数だと仮定する
   let j = 2; // 2からi-1までの数で割って調べる
 
-  while (isPrime && i !== j) { // i=2の場合も素数として扱える
+  while (isPrime && i !== j) {
+    // i=2の場合も素数として扱える
     if (i % j === 0) {
       isPrime = false; // 割り切れたら素数ではないと判定
     }
@@ -148,3 +150,57 @@ for (let i = 2; i <= n; i++) {
 }
 
 console.log(primeMessage.slice(0, -1)); // 最後のカンマを削除して出力
+
+let arr = ["1", "2", "3", "4"];
+console.log(arr);
+arr.push(5); //配列には数字、文字列、配列などなんでもpushできる。
+// ごちゃまぜにするとややこしいので、関連するデータをまとめたほうがいい
+console.log(arr);
+
+console.log(arr.pop()); //最後の要素を抽出して返す
+
+console.log(arr);
+
+console.log(arr.length); //要素の数を返す
+
+console.log(arr.shift()); //最初の要素を抽出して返す　全体が左にシフトする　だからshift
+
+console.log(arr.unshift("1")); //最初に要素を追加する　全体が右にシフトする　だからunshift
+
+console.log(arr);
+
+// delete arr[2];
+//空白が残って気持ち悪くなる　[ '1', '2', <1 empty item>, '4' ]
+
+// splice(start, 個数)
+console.log(arr.splice(0, 2)); //(a,b) a番目からbこの要素を削除し、返す
+
+console.log(arr);
+
+arr.splice(0, 0, "1", "2"); //何も削除せずに追加することも可能
+
+console.log(arr);
+
+//すべてをsplice()で賄うことは技術的にはできるが、
+// 普通は目的が単純なら専用メソッドを使う。
+
+// splice()は好きな位置から、好きな個数を削除・追加したいときに使う
+
+// slice(start, end)
+console.log(arr.slice(1, 3)); //(a,b) a番目からb-a番目を新しい配列としてコピーする
+
+//concat(配列)
+newArr = arr.concat(["3", "4"], [5, 6]); 
+//複数の配列から、新しい配列を作る
+console.log(newArr);
+
+// forEach = 全員に何かする
+arr.forEach(console.log); //forEachはデフォルトで(item, index, array)を渡す
+arr.forEach((item) => {console.log(item);}); //引数を指定することでitemだけ受け取れる
+
+console.log(arr.indexOf("3")); //見つけた場所のインデックスを返す
+console.log(arr.indexOf(false)); //見つからなければ-1を返す
+
+console.log(arr.includes("3")); //みつけたらtrue
+console.log(arr.includes(3)); //みつからなければfalse
+//includesメソッドは　=== で判別しているから正確
