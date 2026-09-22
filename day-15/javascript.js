6a58c69711804e6b4cfd295fd06a6483fe099909
Yukimaru753 <323893844+Yukimaru753@users.noreply.github.com>
2026-09-22 23:53:41 +0900

    Add DOM manipulation exercise

---
+++ day-15/javascript.js
@@ -0,0 +1,36 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+const content2 = document.createElement("p");
+content2.classList.add("content");
+content2.textContent = "Hey I’m red!";
+content2.style.color = "red";
+
+const content3 = document.createElement("h3");
+content3.classList.add("content");
+content3.textContent = "I’m a blue h3!";
+content3.style.color = "blue";
+
+const content4 = document.createElement("div");
+content4.classList.add("content");
+content4.style.backgroundColor = "pink";
+content4.style.border = "1px solid black";
+
+const content5 = document.createElement("h1");
+content5.classList.add("content");
+content5.textContent = "I’m in a div";
+content4.appendChild(content5);
+
+const content6 = document.createElement("p");
+content6.classList.add("content");
+content6.textContent = "ME TOO!";
+content4.appendChild(content6);
+
+
+container.appendChild(content);
+container.appendChild(content2);
+container.appendChild(content3);
+container.appendChild(content4);
